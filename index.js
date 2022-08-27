eb3505d88d1d6140162092739a9d64bc6406b177
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2022-08-27 17:57:49 +0530

    Handling CORS

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const ArticlesRoutes = require('./routes/api/articles');
 
 const app = express();
 
 app.use(helmet());
+app.use(cors({
+    origin: ['http://localhost', 'https://leagues-947c5.web.app']
+}))
 
 connectDB();
 
